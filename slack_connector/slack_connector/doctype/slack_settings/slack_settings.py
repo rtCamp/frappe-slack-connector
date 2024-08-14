39e09c8c5723ead480b0face129e338cf4e1710f
Ananyobrata Pal <ananyo141@gmail.com>
2024-08-14 14:30:36 +0530

    feat: add slack settings doctype

---
+++ slack_connector/slack_connector/doctype/slack_settings/slack_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, rtCamp and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SlackSettings(Document):
+	pass
---
+++ slack_connector/slack_connector/doctype/slack_settings/test_slack_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, rtCamp and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSlackSettings(FrappeTestCase):
+	pass

d94e25e70d438b0f89bbc7b48df8841ea7fd4738
Ananyobrata Pal <ananyo141@gmail.com>
2024-08-23 14:15:23 +0530

    chore: add todo and docstrings

--- slack_connector/slack_connector/doctype/slack_settings/slack_settings.py
+++ slack_connector/slack_connector/doctype/slack_settings/slack_settings.py
@@ -4,6 +4,21 @@
 # import frappe
 from frappe.model.document import Document
 
+# from slack_connector.slack.app import SlackIntegration
+
 
 class SlackSettings(Document):
-	pass
+    # TODO: Implement the following methods
+    def validate(self):
+        """
+        Check if the provided slack channel is valid, taking
+        the slack_app_token and slack_bot_token from the document
+        """
+        pass
+
+    def before_save(self):
+        """
+        Update the slack settings in SlackIntegration class if the document is saved,
+        reset the slack_app instance
+        """
+        pass

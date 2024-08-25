e459bf2286397ec7ffe2ab6f827c445c38389cc4
Ananyobrata Pal <ananyo141@gmail.com>
2024-08-25 20:44:24 +0530

    fix: handle updating slack settings at runtime

--- slack_connector/slack_connector/doctype/slack_settings/slack_settings.py
+++ slack_connector/slack_connector/doctype/slack_settings/slack_settings.py
@@ -4,11 +4,18 @@
 # import frappe
 from frappe.model.document import Document
 
-# from slack_connector.slack.app import SlackIntegration
+from slack_connector.slack.app import SlackIntegration
 
+
+# TODO: Add validation for slack and channel integration
+# Currently we are taking the channel name (not the id), so it is
+# not possible to get the conversations.info api that expects a channel
+# id. One possible solution is to fetch all the paginated channels
+# via conversations.list and then match the channel name with the provided channel name,
+# and then get the channel id (and possibly store in document).
+# This is a costly operation and should be avoided.
 
 class SlackSettings(Document):
-    # TODO: Implement the following methods
     def validate(self):
         """
         Check if the provided slack channel is valid, taking
@@ -18,7 +25,9 @@ class SlackSettings(Document):
 
     def before_save(self):
         """
-        Update the slack settings in SlackIntegration class if the document is saved,
-        reset the slack_app instance
+        Update the slack settings in SlackIntegration class
+        if the document is saved in runtime, reset the slack_app instance
         """
-        pass
+        SlackIntegration.SLACK_APP_TOKEN = self.slack_app_token
+        SlackIntegration.SLACK_BOT_TOKEN = self.slack_bot_token
+        SlackIntegration.SLACK_CHANNEL_ID = self.attendance_channel_id

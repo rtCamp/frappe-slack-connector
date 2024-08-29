8fa6666d5037fbdb48b286a689f33e1fc090db96
Ananyobrata Pal <ananyo141@gmail.com>
2024-08-29 18:41:38 +0530

    fix(50e969c46d395b783b24c234b9684fecc7429c43): remove hooks for updating class attributes

--- slack_connector/slack_connector/doctype/slack_settings/slack_settings.py
+++ slack_connector/slack_connector/doctype/slack_settings/slack_settings.py
@@ -4,9 +4,6 @@
 # import frappe
 from frappe.model.document import Document
 
-from slack_connector.slack.app import SlackIntegration
-
-
 # TODO: Add validation for slack and channel integration
 # Currently we are taking the channel name (not the id), so it is
 # not possible to get the conversations.info api that expects a channel
@@ -15,6 +12,7 @@ from slack_connector.slack.app import SlackIntegration
 # and then get the channel id (and possibly store in document).
 # This is a costly operation and should be avoided.
 
+
 class SlackSettings(Document):
     def validate(self):
         """
@@ -22,12 +20,3 @@ class SlackSettings(Document):
         the slack_app_token and slack_bot_token from the document
         """
         pass
-
-    def before_save(self):
-        """
-        Update the slack settings in SlackIntegration class
-        if the document is saved in runtime, reset the slack_app instance
-        """
-        SlackIntegration.SLACK_APP_TOKEN = self.slack_app_token
-        SlackIntegration.SLACK_BOT_TOKEN = self.slack_bot_token
-        SlackIntegration.SLACK_CHANNEL_ID = self.attendance_channel_id

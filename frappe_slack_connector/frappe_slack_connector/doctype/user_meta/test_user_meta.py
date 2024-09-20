f1a02e867b2c60866ff8645f115c27792b5f0729
zeel-codder <zeelprajapati321@gmail.com>
2024-09-20 14:57:03 +0530

    feat: run the pre-commit on all files

--- frappe_slack_connector/frappe_slack_connector/doctype/user_meta/test_user_meta.py
+++ frappe_slack_connector/frappe_slack_connector/doctype/user_meta/test_user_meta.py
@@ -6,4 +6,4 @@ from frappe.tests.utils import FrappeTestCase
 
 
 class TestUserMeta(FrappeTestCase):
-	pass
+    pass
--- frappe_slack_connector/frappe_slack_connector/doctype/user_meta/user_meta.py
+++ frappe_slack_connector/frappe_slack_connector/doctype/user_meta/user_meta.py
@@ -6,4 +6,4 @@ from frappe.model.document import Document
 
 
 class UserMeta(Document):
-	pass
+    pass
